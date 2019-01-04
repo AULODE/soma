ab8c01a1d5ca74f4894b1a0fd6b5f25545763c89
Kiran Kantesariya <kirankantesariya@gmail.com>
2019-01-04 21:02:47 +0530

    [Add] Customer view Updated

---
+++ sun_customer_details/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Customer Extend',
+    'category': 'Base',
+    'author': 'Kiran Kantesariya',
+    'version': '11.0.1',
+    'description':
+        """
+        Customer Extend
+        """,
+    'depends': ['base','account'],
+    'data': [
+            'views/res_partner_view_extend.xml',
+              ],
+    'installable': True,
+}
