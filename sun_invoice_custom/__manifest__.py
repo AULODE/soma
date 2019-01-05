a7848d5553a13f12bbe70ff80d651119b47e2175
Kiran Kantesariya <kirankantesariya@gmail.com>
2019-01-05 17:13:48 +0530

    [IMP] sale, invoice report, payment extend, Partnr

--- sun_invoice_custom/__manifest__.py
+++ sun_invoice_custom/__manifest__.py
@@ -1,10 +1,10 @@
 {
     'name': 'Account customization',
-    'version': '12.0.1',
+    'version': '11.0.1',
     'category': 'Accounting',
     'author': 'Kiran Kantesariya',
     'summary': 'Account Customization',
-    'depends': ['account'],
+    'depends': ['account','account_cancel'],
     'data': ['views/sequence_file.xml',
              'views/report_invoice.xml'],
     'installable': True,
---
+++ sun_report_custom/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Generic Report customization',
+    'version': '11.0.1',
+    'category': 'Sales Management',
+    'author': 'Kiran Kantesariya',
+    'summary': 'Generic Report Customization',
+    'depends': ['sale_management'],
+    'data': ['views/sale_report.xml',
+             'views/report.xml',
+             'views/res_company_view.xml',
+            ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
