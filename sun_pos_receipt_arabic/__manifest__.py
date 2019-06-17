768667a04237a35d7127854bfde2933a3f9941f8
Kiran Kantesariya <kirankantesariya@gmail.com>
2019-06-17 16:56:20 +0530

    [Add]add module Pos Arabic Receipt

---
+++ sun_pos_receipt_arabic/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'POS Arabic Receipt / POS Customized Receipt(Arabic Receipt)',
+    'version': '12.0.5',
+    'sequence': 1,
+    'category': 'Point of Sale',
+    'summary': 'POS Arabic Receipt',
+    'author': 'Kiran Kantesariya',
+    'price': 7,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+    'description': """
+POS Customized Receipt(Arabic Receipt)
+        """,
+    'depends': ['point_of_sale'],
+    'qweb': ['static/src/xml/templates.xml'],
+    'data': ['views/pos_receipt_template.xml'],
+    'images': ['static/description/main_screenshot.jpg'],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+
